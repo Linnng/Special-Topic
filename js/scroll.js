59760b2d8cc93fe1bfeff6c7385af4e3acdb94d2
HsiangLing <h.ling1075@gmail.com>
2019-09-02 09:40:30 +0800

    scroll js

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+$('#firstBtn_next').click(function(){
+    $('html,body').animate({scrollTop: '1200px'}, 300)
+});
+
+$('#secondBtn_last').click(function(){
+    $('html,body').animate({scrollTop: '0px'}, 300)
+});
+$('#secondBtn_next').click(function(){
+    $('html,body').animate({scrollTop: '2300px'}, 300)
+});
+
+$('#thirdBtn_last').click(function(){
+    $('html,body').animate({scrollTop: '1200px'}, 300)
+});
+$('#thirdBtn_next').click(function(){
+    $('html,body').animate({scrollTop: '3600px'}, 300)
+});
+// give the btns idname
